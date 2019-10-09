9552aabcd2c34fe7ada4edf63056f4355cac2658
Matthew Birch <matthew.birch@accedo.tv>
2019-10-09 08:31:30 +0100

    Implemented smooth scrolling to each anchor.

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -4,6 +4,7 @@ define('components/nav', ['jquery', 'helper/util'], function($, util) {
     var $trigger = $('.trigger')
     var $frame = $('html, body')
     var $a = $('a[href*="#"]')
+    var $window = $(window)
 
     var timer = null
 
@@ -49,7 +50,42 @@ define('components/nav', ['jquery', 'helper/util'], function($, util) {
         }
     }
 
+    // About - 550px
+    // Projects - 500px
+    // Blog - 600px
+    // Contact - 350px
+
+    function setActiveNavItem() {
+        var about = getSection('#about')
+        var projects = getSection('#projects')
+        var blog = getSection('#blog')
+        var contact = getSection('#contact')
+
+        var scrollPositionFromTop = $(this).scrollTop()
+
+        $a.removeClass('active')
+
+        if (scrollPositionFromTop > about.sectionY && scrollPositionFromTop < projects.sectionY) {
+            console.log('Projects Section')
+            projects.navItem.addClass('active')
+        } else if (scrollPositionFromTop > projects.sectionY && scrollPositionFromTop < blog.sectionY) {
+            console.log('Blog Section')
+            blog.navItem.addClass('active')
+        } else if (scrollPositionFromTop > blog.sectionY && scrollPositionFromTop < contact.sectionY) {
+            console.log('Contact Section')
+            contact.navItem.addClass('active')
+        } else {
+            console.log('About Section')
+            about.navItem.addClass('active')
+        }
+    }
+
+    function getSection(id) {
+        return {sectionY: $(id).offset().top, navItem: $('a[href=" + id + "]')}
+    }
+
     prepareDesktopNav()
     $trigger.on('click', showMenu)
     $a.on('click', smoothScrollToAnchor)
+    $window.on('scroll', setActiveNavItem)
 })
